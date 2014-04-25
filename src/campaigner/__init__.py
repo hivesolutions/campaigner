04f420058d5c617c9b12aca86afeba2fa66f62ea
joamag <joamag@gmail.com>
2014-04-25 16:22:30 +0100

    new init file

--- src/campaigner/__init__.py
+++ src/campaigner/__init__.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-import controllers
-import models
-import main
+from . import controllers
+from . import models
+from . import main
 
-from controllers import *
-from models import *
-from main import *
+from .controllers import *
+from .models import *
+from .main import *
