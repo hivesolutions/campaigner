594898a39ef3a01e4e0e0e7d9d50649c765071fe
joamag <joamag@gmail.com>
2014-04-28 17:55:44 +0100

    small code change

--- src/campaigner/controllers/campaign.py
+++ src/campaigner/controllers/campaign.py
@@ -30,7 +30,8 @@ class CampaignController(appier.Controller):
         subscription = campaigner.Subscription.new()
         subscription.campaign = name
         try: subscription.save()
-        except appier.OperationalError:
+        except appier.OperationalError as exception:
+            print(exception)
             if not redirect_url: raise
             return self.redirect(redirect_url, result = "error")
         return self.redirect(redirect_url, result = "success")
