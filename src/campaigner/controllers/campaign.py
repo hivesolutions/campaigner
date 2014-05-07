fdf6230702f85d0e34e93d757b53052d6f9e4ccf
joamag <joamag@gmail.com>
2014-05-07 17:01:49 +0100

    new secrity and subscription in csv support

--- src/campaigner/controllers/campaign.py
+++ src/campaigner/controllers/campaign.py
@@ -11,18 +11,30 @@ class CampaignController(appier.Controller):
         appier.Controller.__init__(self, owner, *args, **kwargs)
 
     @appier.route("/campaigns.json", "GET", json = True)
+    @appier.ensure(token = "admin")
     def list_json(self):
         object = appier.get_object(alias = True, find = True)
         campaigns = campaigner.Campaign.find(map = True, **object)
         return campaigns
 
     @appier.route("/campaigns/<name>/subscriptions.json", "GET", json = True)
+    @appier.ensure(token = "admin")
     def subscriptions_json(self, name):
         object = appier.get_object(alias = True, find = True)
         subscriptions = campaigner.Subscription.find(campaign = name, map = True, **object)
         return subscriptions
 
+    @appier.route("/campaigns/<name>/subscriptions.csv", "GET")
+    @appier.ensure(token = "admin")
+    def subscriptions_csv(self, name):
+        object = appier.get_object(alias = True, find = True)
+        subscriptions = campaigner.Subscription.find(campaign = name, map = True, **object)
+        result = appier.serialize_csv(subscriptions)
+        self.content_type("text/csv")
+        return result
+
     @appier.route("/campaigns/<name>/subscriptions", "POST", json = True)
+    @appier.ensure(token = "admin")
     def create_subscription(self, name):
         campaign = campaigner.Campaign.get(name = name)
         redirect_url = campaign.redirect_url if campaign else None
