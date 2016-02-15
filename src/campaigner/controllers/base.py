f37d9b0592e094c74968e7c43377c141884bf430
joamag <joamag@hive.pt>
2016-02-15 16:37:43 +0000

    removed extra index template

--- src/campaigner/controllers/base.py
+++ src/campaigner/controllers/base.py
@@ -4,8 +4,4 @@
 import appier
 
 class BaseController(appier.Controller):
-
-    @appier.route("/", "GET")
-    @appier.route("/index", "GET")
-    def index(self):
-        return self.template("index.html.tpl")
+    pass
